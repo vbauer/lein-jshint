fb43294a410f9647bdf53124cf5b7721aaf38e63
Vladislav Bauer <bauer.vlad@gmail.com>
2014-05-12 19:13:03 +0700

    lein-release plugin: bumped version from 0.1.0 to 0.1.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-jshint "0.1.0"
+(defproject lein-jshint "0.1.1-SNAPSHOT"
   :description "A Leiningen plugin that allows to do static analysis for JavaScript files."
   :url "https://github.com/vbauer/lein-jshint"
   :license {:name "Eclipse Public License"
