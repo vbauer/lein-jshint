11efc9bf2d8ef5f38d9a542e7331b8f29ef83ec5
Vladislav Bauer <bauer.vlad@gmail.com>
2015-03-28 14:37:36 +0600

    lein-release plugin: bumped version from 0.1.10 to 0.1.11-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-jshint "0.1.10"
+(defproject lein-jshint "0.1.11-SNAPSHOT"
   :description "A Leiningen plugin for running JS code through JSHint."
   :url "https://github.com/vbauer/lein-jshint"
   :license {:name "Eclipse Public License"
